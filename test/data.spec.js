269ac984f853ea149e039f60968048eed2fe191c
joyce <joyce.mbt@gmail.com>
2022-08-25 14:21:30 -0300

    criei o teste da funcao computerstats

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,60 +1,60 @@
-import { filtroDoSelect, ordenandoCampeoes} from '../src/data.js';
+import { sortData, computerStats} from '../src/data.js';
 
 
-describe('filtroDoSelect', () => {
-  it('Deveria ser uma constante', () => {
-    expect(typeof filtroDoSelect).toBe('function');
-  });
+// describe('filterData', () => {
+//   it('Deveria ser uma constante', () => {
+//     expect(typeof filtroDoSelect).toBe('function');
+//   });
 
 
-  it('Deveria retornar os campeões que contém a tag Suporte', () => {
-    const dados = [
-      {
-        name: "Nami",
-        tags: [
-          "Support",
-          "Mage"
-        ]
-      },
-      {
-        name: "Blitzcrank",
-        tags: [
-          "Tank",
-          "Fighter"
-        ]
-      },
-      {
-        name: "Taric",
-        tags: [
-          "Support",
-          "Fighter"
-        ]
-      }
-    ];
+//   it('Deveria retornar os campeões que contém a tag Suporte', () => {
+//     const dados = [
+//       {
+//         name: "Nami",
+//         tags: [
+//           "Support",
+//           "Mage"
+//         ]
+//       },
+//       {
+//         name: "Blitzcrank",
+//         tags: [
+//           "Tank",
+//           "Fighter"
+//         ]
+//       },
+//       {
+//         name: "Taric",
+//         tags: [
+//           "Support",
+//           "Fighter"
+//         ]
+//       }
+//     ];
     
-    const resultado = [
-      {
-        tags: [
-          "Support",
-          "Mage"
-        ],
-        name: "Nami"
-      },
-      {
-        name: "Taric",
-        tags: [
-          "Support",
-          "Fighter"
-        ]
-      }
-    ];
-    expect(filtroDoSelect(dados, "Support")).toStrictEqual(resultado);
-  });
-});
+//     const resultado = [
+//       {
+//         tags: [
+//           "Support",
+//           "Mage"
+//         ],
+//         name: "Nami"
+//       },
+//       {
+//         name: "Taric",
+//         tags: [
+//           "Support",
+//           "Fighter"
+//         ]
+//       }
+//     ];
+//     expect(filterData(dados, "Support")).toStrictEqual(resultado);
+//   });
+// });
 
-describe('ordenandoCampeoes', () => {
+describe('sortData', () => {
   it('Deveria ser uma função', () => {
-    expect(typeof ordenandoCampeoes).toBe('function');
+    expect(typeof sortData).toBe('function');
   });
 
 
@@ -87,6 +87,62 @@ describe('ordenandoCampeoes', () => {
         name: "Blitzcrank"
       }
     ];
-    expect(ordenandoCampeoes(dados)).toStrictEqual(resultado)
+    expect(sortData(dados)).toStrictEqual(resultado)
+  });
+})
+
+describe ('computerStats', () => {
+  it('Deveria retornar a média do ataque, magia ou defesa do campeão de acordo com sua função', () => {
+    const dados = 
+    [
+      {
+        name: "Nami",
+        tags: [
+          "Support",
+          "Mage"
+        ],
+        info: {
+          attack: 8,
+          defense: 4,
+          magic: 3,
+          difficulty: 4
+        }
+      },
+      {
+        name: "Blitzcrank",
+        tags: [
+          "Tank",
+          "Fighter"
+        ],
+        info: {
+          attack: 3,
+          defense: 4,
+          magic: 8,
+          difficulty: 5
+        }
+      },
+      {
+        name: "Taric",
+        tags: [
+          "Support",
+          "Fighter"
+        ],
+        info: {
+          attack: 3,
+          defense: 4,
+          magic: 8,
+          difficulty: 5
+        }
+      }
+    ];
+
+    const resultado =
+      {
+        type: 'defense',
+        media: '4.0'
+      }
+    ;
+    expect(computerStats('Support', dados)).toStrictEqual(resultado);
+
   });
 })
