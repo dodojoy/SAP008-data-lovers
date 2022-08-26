a81437bd8145214c76634764cf11ec0ecef2a827
joyce <joyce.mbt@gmail.com>
2022-08-25 23:15:00 -0300

    criei o teste da função filtro por tag e por dificuldade

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,56 +1,112 @@
-import { sortData, computerStats} from '../src/data.js';
+import {filterDataByTag, filterDataByDifficulty, sortData, computerStats} from '../src/data.js';
 
 
-// describe('filterData', () => {
-//   it('Deveria ser uma constante', () => {
-//     expect(typeof filtroDoSelect).toBe('function');
-//   });
-
-
-//   it('Deveria retornar os campeões que contém a tag Suporte', () => {
-//     const dados = [
-//       {
-//         name: "Nami",
-//         tags: [
-//           "Support",
-//           "Mage"
-//         ]
-//       },
-//       {
-//         name: "Blitzcrank",
-//         tags: [
-//           "Tank",
-//           "Fighter"
-//         ]
-//       },
-//       {
-//         name: "Taric",
-//         tags: [
-//           "Support",
-//           "Fighter"
-//         ]
-//       }
-//     ];
+describe('filterDataByTag', () => {
+  
+it('Deveria retornar os campeões que contém a tag Suporte', () => {
+    const dados = [
+      {
+        name: "Nami",
+        tags: [
+          "Support",
+          "Mage"
+        ]
+      },
+      {
+        name: "Blitzcrank",
+        tags: [
+          "Tank",
+          "Fighter"
+        ]
+      },
+      {
+        name: "Taric",
+        tags: [
+          "Support",
+          "Fighter"
+        ]
+      }
+    ];
     
-//     const resultado = [
-//       {
-//         tags: [
-//           "Support",
-//           "Mage"
-//         ],
-//         name: "Nami"
-//       },
-//       {
-//         name: "Taric",
-//         tags: [
-//           "Support",
-//           "Fighter"
-//         ]
-//       }
-//     ];
-//     expect(filterData(dados, "Support")).toStrictEqual(resultado);
-//   });
-// });
+    const resultado = [
+      {
+        tags: [
+          "Support",
+          "Mage"
+        ],
+        name: "Nami"
+      },
+      {
+        name: "Taric",
+        tags: [
+          "Support",
+          "Fighter"
+        ]
+      }
+    ];
+    expect(filterDataByTag(dados, "Support")).toStrictEqual(resultado);
+  });
+});
+
+describe('filterDataByDifficulty', () => {
+  it('Deveria retornar os campeões com base na sua dificuldade', () => {
+    const dados = [
+      {
+        name: "Nami",
+        tags: [
+          "Support",
+          "Mage"
+        ],
+        info: {
+          attack: 8,
+          defense: 4,
+          magic: 3,
+          difficulty: 4
+        }
+      },
+      {
+        name: "Blitzcrank",
+        tags: [
+          "Tank",
+          "Fighter"
+        ],
+        info: {
+          attack: 3,
+          defense: 4,
+          magic: 8,
+          difficulty: 5
+        }
+      },
+      {
+        name: "Taric",
+        tags: [
+          "Support",
+          "Fighter"
+        ],
+        info: {
+          attack: 3,
+          defense: 4,
+          magic: 8,
+          difficulty: 5
+        }
+      }];
+    
+    const resultado = [{
+      name: "Nami",
+      tags: [
+        "Support",
+        "Mage"
+      ],
+      info: {
+        attack: 8,
+        defense: 4,
+        magic: 3,
+        difficulty: 4
+      }
+    }];
+    expect(filterDataByDifficulty(dados, 'baixo')).toStrictEqual(resultado);
+  })
+})
 
 describe('sortData', () => {
   it('Deveria ser uma função', () => {
