a9651032e2b7e5b5f3609382efa4e7f3556ba085
joyce <joyce.mbt@gmail.com>
2022-08-30 08:06:11 -0300

    arquivo de tradução para mostrar a média como no protótipo

---
+++ src/translate.js
@@ -0,0 +1,14 @@
+const info = {
+    'attack': 'ataque',
+    'magic': 'magia',
+    'defense': 'defesa',
+    'Assassin': 'assassinos',
+    'Mage': 'magos',
+    'Marksman': 'atiradores',
+    'Fighter': 'lutadores',
+    'Support': 'suportes',
+    'Tank': 'tanques'
+};
+
+export default info;
+
